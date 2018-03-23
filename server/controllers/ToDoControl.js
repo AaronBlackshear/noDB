721b1d5c223006aafa526b90cd6c845f5a09b492
OGCypherpunk <chuck.devmtn@gmail.com>
2018-03-23 12:13:22 -0500

    file structure complete

---
+++ server/controllers/ToDoControl.js
@@ -0,0 +1,11 @@
+//arr variable === is my start my to-do list
+
+var todoList = [ ];
+
+
+module.exports = {
+    //this exports this stuff to the index.js 
+    //fron todocontrol to the server/index and then to the front end
+    
+    
+}
---
+++ server/index.js
@@ -0,0 +1,1 @@
+//this is where my Node server code goes
